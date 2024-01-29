ec0b8da73e28f74ce64fc8db2e7560c1cf287cf1
Rafael Garcia <rafaelgarciaworks@gmail.com>
2024-01-28 22:35:07 -0600

    Add entity framework and config

---
+++ Parking App/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parking_App
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
